61d3c9067e952c2de88b0750b1680e10287f6f4c
Erik Parmann <epar@statoil.com>
2018-09-24 13:38:14 +0200

    Add documentation to lisp-code

--- lisp-18/final-code.lisp
+++ lisp-18/final-code.lisp
@@ -1,20 +1,29 @@
 (defvar *test-name* nil)
 
 (defmacro deftest (name parameters &body body)
+  "Define a test (a function) with the given name and parameter,
+  and the tests in body"
   `(defun ,name ,parameters
      (let ((*test-name* (append *test-name* (list ',name)) ))
        ,@body)))
 
 (defmacro check (&body forms)
+  "Given a list of boolean expressions, returns a form which reports
+   fail or pass depending on their truth values. Returns t if and only if
+   they all evalute to t"
   `(combine-results
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defmacro combine-results (&body forms)
+  "Returns a form which is similar to 'and', but evaluates all expressions
+   independently of proceeding truth values."
   `(let ((result t))
      ,@(loop for f in forms collect `(if (not ,f)  (setf result nil)))
      result))
 
 (defun report-result (result form)
+  "Reports the result of an evaluated boolean expr, with its original expr.
+   Returns the result of the evaluted expr."
   (progn
     (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
     result))
